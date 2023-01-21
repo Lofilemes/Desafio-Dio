ab8fabb7422dfaea7951c702a09cad6065625830
Lorrana Filemes <lorranafilemes17@gmail.com>
2023-01-21 18:21:50 -0300

    desafio concluido

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using DesafioPOO.Models;
+
+Smartphone nokia = new Nokia (numero: "123", modelo: "Modelo 1", imei: "111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Smartphone iphone = new Iphone(numero: "789", modelo: "Modelo xr", imei: "999", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
